7d3d706bc36ec4b03a3b191492bc954b0577abb0
Randy Rowland <randy.rowland@disroot.org>
2021-11-30 21:33:55 -0700

    Test: Single value to empty collection
    Create a test to verify adding a single value to an empty collection
    
    Test: Add more tests
    
    Completed all assigned tests but need to create own tests.

--- test.cpp
+++ test.cpp
@@ -1,3 +1,24 @@
+/************************************************************************
+ Program:       Unit Testing
+ Author:        Randall Rowland (https://github.com/rowland007)
+ Class:         CS-405-T2676 Secure Coding 21EW2
+ Instructor:    Mr. Trevor Hodde
+ Date:          2021-11-30
+ Description:   A large banking web application is implementing unit tests
+                as a best practice in the SDLC.
+ Input:
+ Output:
+ Known bugs:    
+ Missing features:
+    The source code has been commented with TODOs to explain the detailed rules
+    you must follow. There are comments that mark code that must be changed.
+    Remember to leverage capabilities provided by the  standard C++ library to help you achieve success.
+ License:       Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
+ Modifications:
+   Date                      Comment
+ ---------   ------------------------------------------------
+ 
+ ************************************************************************/
 // Uncomment the next line to use precompiled headers
 #include "pch.h"
 // uncomment the next line if you do not use precompiled headers
@@ -85,41 +106,111 @@ TEST_F(CollectionTest, AlwaysFail)
     FAIL();
 }
 
-// TODO: Create a test to verify adding a single value to an empty collection
+//  Create a test to verify adding a single value to an empty collection
 TEST_F(CollectionTest, CanAddToEmptyVector)
 {
     // is the collection empty?
+    ASSERT_TRUE(collection->empty());
     // if empty, the size must be 0
+    ASSERT_EQ(collection->size(), 0);
 
     add_entries(1);
 
     // is the collection still empty?
+    ASSERT_FALSE(collection->empty());
     // if not empty, what must the size be?
+    ASSERT_GT(collection->size(), 0);
 }
 
 // TODO: Create a test to verify adding five values to collection
 TEST_F(CollectionTest, CanAddFiveValuesToVector)
 {
     add_entries(5);
+    ASSERT_EQ(collection->size(), 5);
 }
 
 // TODO: Create a test to verify that max size is greater than or equal to size for 0, 1, 5, 10 entries
+TEST_F(CollectionTest, MaxIsGreaterThanOrEqualToSizeTest) {
+    add_entries(11);
+
+    ASSERT_TRUE(collection->max_size() >= 0);
+    ASSERT_TRUE(collection->max_size() >= 1);
+    ASSERT_TRUE(collection->max_size() >= 5);
+    ASSERT_TRUE(collection->max_size() >= 10);
+}
 
 // TODO: Create a test to verify that capacity is greater than or equal to size for 0, 1, 5, 10 entries
+TEST_F(CollectionTest, CapacityIsGreaterThanOrEqualToSizeTest) {
+    add_entries(11);
+
+    ASSERT_TRUE(collection->capacity() >= 0);
+    ASSERT_TRUE(collection->capacity() >= 1);
+    ASSERT_TRUE(collection->capacity() >= 5);
+    ASSERT_TRUE(collection->capacity() >= 10);
+}
 
 // TODO: Create a test to verify resizing increases the collection
+TEST_F(CollectionTest, ResizeIncreaseTest) {
+    add_entries(1);
+
+    size_t previousSize = collection->size();
+    collection->resize(20);
+    ASSERT_TRUE(collection->size() > previousSize);
+}
 
 // TODO: Create a test to verify resizing decreases the collection
+TEST_F(CollectionTest, ResizeDecreaseTest) {
+    add_entries(20);
+
+    size_t previousSize = collection->size();
+    collection->resize(1);
+    ASSERT_TRUE(collection->size() < previousSize);
+}
 
 // TODO: Create a test to verify resizing decreases the collection to zero
+TEST_F(CollectionTest, ResizeIsZeroTest) {
+    add_entries(20);
+
+    size_t previousSize = collection->size();
+    collection->resize(0);
+    ASSERT_TRUE(collection->size() == 0);
+}
 
 // TODO: Create a test to verify clear erases the collection
+TEST_F(CollectionTest, VerifyCollectionClearedTest) {
+    add_entries(20);
+
+    collection->clear();
+    ASSERT_TRUE(collection->size() == 0);
+}
 
 // TODO: Create a test to verify erase(begin,end) erases the collection
+TEST_F(CollectionTest, VerifyEraseBeginEndTest) {
+    add_entries(20);
+
+    collection->erase(collection->begin(), collection->end());
+    ASSERT_TRUE(collection->size() == 0);
+}
 
 // TODO: Create a test to verify reserve increases the capacity but not the size of the collection
+TEST_F(CollectionTest, ReserveIncreaseCapacityNotSizeTest) {
+    add_entries(20);
+
+    size_t prevCapacity = collection->capacity();
+    size_t prevSize = collection->size();
+
+    collection->reserve(30);
+
+    ASSERT_TRUE(collection->size() == prevSize);
+    ASSERT_TRUE(collection->capacity() > prevCapacity);
+}
 
 // TODO: Create a test to verify the std::out_of_range exception is thrown when calling at() with an index out of bounds
 // NOTE: This is a negative test
+TEST_F(CollectionTest, IndexOutOfRangeTest) {
+    std::vector<int> myvector(10);
+    EXPECT_THROW(myvector.at(20), std::out_of_range);
+    ASSERT_THROW(myvector.at(20), std::out_of_range);
+}
 
 // TODO: Create 2 unit tests of your own to test something on the collection - do 1 positive & 1 negative
